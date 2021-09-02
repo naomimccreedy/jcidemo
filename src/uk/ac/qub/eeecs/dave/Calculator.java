7e430e571ae4a83e4b6e15b98a4cd6b8095bec04
Naomi McCreedy <63666510+Naomim43@users.noreply.github.com>
2021-09-02 16:03:53 +0100

    newlines

--- src/uk/ac/qub/eeecs/dave/Calculator.java
+++ src/uk/ac/qub/eeecs/dave/Calculator.java
@@ -1,11 +1,14 @@
 package uk.ac.qub.eeecs.dave;
 
 public class Calculator {
+	
 	public int add(int a, int b) {
+		System.out.println("\n");
 		return a+b;
 	}
 	
 	public int sub(int a, int b) {
+		System.out.println("\n");
 		return a-b;
 	}
 	
